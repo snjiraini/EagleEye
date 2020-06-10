a83d19cb66c9d5b2c3d00b07950eff789ee2b04e
snjiraini <snjiraini@gmail.com>
2020-06-10 16:34:29 +0300

    Add project files.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace EagleEye
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.office2010BlackTheme1 = new Telerik.WinControls.Themes.Office2010BlackTheme();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1805, 1000);
+            this.Name = "frmMain";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.Text = "EagleEye";
+            this.ThemeName = "Office2010Black";
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.Themes.Office2010BlackTheme office2010BlackTheme1;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls.UI;
+using Telerik.WinControls;
+
+namespace EagleEye
+{
+    public partial class frmMain : Telerik.WinControls.UI.RadForm
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            ThemeResolutionService.ApplicationThemeName = "Office2010Black";
+        }
+    }
+}
